6a9c990a1f3575acd066d79d8b2dbd9b0a037df3
nxbeyxnd <unblocksteam@gmail.com>
2022-07-06 11:58:16 +0300

    1.Docker compose update 2.Added frontEnd pages

---
+++ front-service/src/main/java/com/geekbrains/frontservice/FrontServiceApplication.java
@@ -0,0 +1,13 @@
+package com.geekbrains.frontservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrontServiceApplication.class, args);
+    }
+
+}
